b36ab4f5bec3284ccf770eecca99b843ca44a06d
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-01 10:04:07 -0600

    inch has nothing to suggest

--- app/models/user_model.rb
+++ app/models/user_model.rb
@@ -1,2 +1,7 @@
+# Columns: id, email, password, first_name, last_name
+#
+# Relationship to friends: user_id on friends table
+# Relationship to ratings: user_id on ratings table
+
 class User < ActiveRecord::Base
 end

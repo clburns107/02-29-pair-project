04251027cf938a94ab939a7ba85bcd8e8f0d2a80
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-01 17:40:26 -0600

    view and controller for my friends added

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,12 @@
-MyApp.get "/add_friend/:id" do
+MyApp.post "/add_friend/:id" do
   @new_friend = Friend.new
   @new_friend.friend_id = params[:id]
   @new_friend.user_id = session["user_id"]
   @new_friend.save
+  erb :"friends/users_friends"
+end
+
+MyApp.get "/my_friends" do
+  @all_my_friends = Friend.where(user_id: session["user_id"])
   erb :"friends/users_friends"
 end

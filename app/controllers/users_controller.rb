c734a58485c90af5a571d7bbd8111963fe9b9c77
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-01 10:00:13 -0600

    edited rubocop fixes for spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
-MyApp.get "/dashboard"  do
-  
+MyApp.get "/dashboard" do
   erb :"users/dashboard"
 end
 
-MyApp.get "/users/new"  do
-  
+MyApp.get "/users/new" do
   erb :"users/new"
 end
 
-MyApp.get "/users/edit"  do
-  
+MyApp.get "/users/edit" do
   erb :"users/edit"
 end
 

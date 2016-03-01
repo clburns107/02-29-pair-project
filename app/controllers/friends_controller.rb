6f4cb9ddf46dd9c5f42ef3165904816e0edfb5f1
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-01 17:27:15 -0600

    add friend button saves friend in db

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,7 @@
 MyApp.get "/add_friend/:id" do
-  
+  @new_friend = Friend.new
+  @new_friend.friend_id = params[:id]
+  @new_friend.user_id = session["user_id"]
+  @new_friend.save
   erb :"friends/users_friends"
 end
---
+++ app/models/friend.rb
@@ -0,0 +1,8 @@
+# Columns: user_id, friend_id
+#
+# Relationship to users: id on users table
+
+class Friend < ActiveRecord::Base
+
+  
+end

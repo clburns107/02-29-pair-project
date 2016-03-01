46af2e449b74a634a909761fa7d08a428c9da7da
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-01 11:09:09 -0600

    updated config file

--- db/schema.rb
+++ db/schema.rb
@@ -15,19 +15,6 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "assignees", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "todo_id"
-    t.integer  "user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-  end
-
   create_table "friends", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -50,21 +37,9 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer  "movie_id"
   end
 
-  create_table "todos", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "title"
-    t.text     "description"
-    t.boolean  "completed"
-    t.integer  "assignor_id"
-    t.integer  "creator_id"
-    t.integer  "category_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name"
     t.string   "email"
     t.string   "password"
     t.string   "first_name"

e43fc0f1604e6f534c610d41a4c9269a4b29e1bc
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-01 09:42:21 -0600

    new user create complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+MyApp.get "/dashboard"  do
+  
+  erb :"users/dashboard"
+end
+
+MyApp.get "/users/new"  do
+  
+  erb :"users/new"
+end
+
+MyApp.get "/users/edit"  do
+  
+  erb :"users/edit"
+end
+
+MyApp.post "/submit/new_user" do
+  @new_user = User.new
+  @new_user.first_name = params[:first_name]
+  @new_user.last_name = params[:last_name]
+  @new_user.email = params[:email]
+  @new_user.password = params[:password]
+  @new_user.save
+
+  if @new_user.password = params[:password]
+    session["user_id"] = @new_user.id
+    @user_reviews = Review.where(user_id: session["user_id"])
+    erb :"users/dashboard"
+  else
+    erb :"login_error"
+  end
+end

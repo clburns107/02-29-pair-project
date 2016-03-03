ba9af8a8018fe3efa3d76a63d6c2f364d77880f3
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-03 12:10:25 -0600

    we can Review an individual friend's movie ratings

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,3 +1,5 @@
+# CREATES 
+
 MyApp.post "/add_friend/:id" do
   @new_friend = Friend.new
   @new_friend.friend_id = params[:id]
@@ -6,13 +8,16 @@ MyApp.post "/add_friend/:id" do
   redirect "/my_friends"
 end
 
+# View a list of all the users the current_user is friends with.
 MyApp.get "/my_friends" do
   @all_my_friends = Friend.where(user_id: session["user_id"])
   erb :"friends/users_friends"
 end
 
+# View
 MyApp.get "/friends_dashboard/:friend_id" do
-  @friend_id = Friend.find_by_id(params[:friend_id])
+  # @friend_id = Friend.find_by_id(params[:friend_id])
+  @friend = User.find_by_id(params[:friend_id])
   @ratings = Rating.where(user_id: params[:friend_id])
   erb :"friends/friends_dashboard"
 end
